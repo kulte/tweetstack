119198feb51fdc341cd4a6e82091a4c93d6f8882
Zachary Friedman <zafriedman@gmail.com>
2013-01-21 10:19:29 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetstack::Application.config.secret_token = '3991dd779671a96f5ab13a7c5da4c551b958ec2233ad785d34b1a070ba2f5a06fbbc5cd1bfd7c72534534b5365e92efcd9572060fce52bb3a1b69bfca1f6a2a1'
